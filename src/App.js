6e6cb95d94f62f3b8ad6f924cde43ba6ab423d2f
Murat Demirtas <muratdemirtas2001@gmail.com>
2021-05-18 12:28:38 +0100

    Adding css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { useState } from "react";
-import MenuBar from "./MenuBar"
+import MenuBar from "./MenuBar";
 import Quote from "./Quote";
 import ButtonQuote from "./ButtonQuote";
 function App() {
@@ -8,11 +8,13 @@ function App() {
 
   return (
     <>
-      <MenuBar/>
-    <div className="content">
-      <Quote click={click} />
-      <ButtonQuote setClick={setClick} />
-    </div>
+      <MenuBar />
+      <div className="content-wrapper">
+        <div className="content">
+          <Quote click={click} />
+        </div>
+        <ButtonQuote setClick={setClick} />
+      </div>
     </>
   );
 }
